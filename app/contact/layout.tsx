375645446a78b35757fded2bddcb6e525e93f1c5
Ankit Soni <ankit@srisaiconsultancyinfosoft.in>
2026-02-25 14:33:12 +0530

    #

--- app/contact/layout.tsx
+++ app/contact/layout.tsx
@@ -1,5 +1,10 @@
 import Footer from "@/components/Footer/footer";
 import Navbar from "@/components/Header/navbar";
+import type { Metadata } from "next";
+ 
+export const metadata: Metadata = {
+  title: "Contact",
+};
 
 function layout({ children }: { children: React.ReactNode }) {
   return (
